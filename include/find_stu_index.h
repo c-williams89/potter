e92780ab70529d42eaf659322bc1405a8c90ea09
clayton williams <clayton.e.williams15.mil@mail.mil>
2023-06-09 13:19:25 -0400

    Created test plan.

--- include/find_stu_index.h
+++ include/find_stu_index.h
@@ -1,4 +1,4 @@
 #ifndef FIND_STU_INDEX_H
 	#define FIND_STU_INDEX_H
-	void find_stu_index(student *stu_list, char *person_to_find);
+		void find_stu_index(student * stu_list, char *person_to_find);
 #endif
